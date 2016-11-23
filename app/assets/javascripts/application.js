3cf0fda4dbde8b8eaad89c431c593ba78cd75211
Alex <alexduval71@gmail.com>
2016-11-23 17:18:51 +0100

    project index and show without style

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,24 +3,24 @@
 //= require bootstrap-sprockets
 //= require_tree .
 
-var $grid = $('.project-cards').isotope({
-  itemSelector: '.project-card',
-  layoutMode: 'vertical'
-});
+// var $grid = $('.project-cards').isotope({
+//   itemSelector: '.project-card',
+//   layoutMode: 'vertical'
+// });
 
 // bind filter button click
-$('.filters-button-group').on( 'click', 'button', function() {
-  var filterValue = $( this ).attr('data-filter');
-  // use filterFn if matches value
-  filterValue = filterFns[ filterValue ] || filterValue;
-  $grid.isotope({ filter: filterValue });
-});
-
-// change is-checked class on buttons
-$('.button-group').each( function( i, buttonGroup ) {
-  var $buttonGroup = $( buttonGroup );
-  $buttonGroup.on( 'click', 'button', function() {
-    $buttonGroup.find('.is-checked').removeClass('is-checked');
-    $( this ).addClass('is-checked');
-  });
-});
+// $('.filters-button-group').on( 'click', 'button', function() {
+//   var filterValue = $( this ).attr('data-filter');
+//   // use filterFn if matches value
+//   filterValue = filterFns[ filterValue ] || filterValue;
+//   $grid.isotope({ filter: filterValue });
+// });
+//
+// // change is-checked class on buttons
+// $('.button-group').each( function( i, buttonGroup ) {
+//   var $buttonGroup = $( buttonGroup );
+//   $buttonGroup.on( 'click', 'button', function() {
+//     $buttonGroup.find('.is-checked').removeClass('is-checked');
+//     $( this ).addClass('is-checked');
+//   });
+// });
