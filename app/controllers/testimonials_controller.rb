7ec89fe7bda4750b19a67bb83d53278f7ce788eb
Alex <alexduval71@gmail.com>
2016-11-29 16:43:25 +0100

    crud testimonial controller

--- app/controllers/testimonials_controller.rb
+++ app/controllers/testimonials_controller.rb
@@ -1,8 +1,29 @@
 class TestimonialsController < ApplicationController
-
+skip_before_action :authenticate_user!, only: [:index]
 
   def index
     @testimonials = Testimonial.all
+  end
+
+  def new
+    @testimonial = Testimonial.new
+  end
+
+  def create
+    @testimonial = Testimonial.new(testimonial_params)
+    if @testimonial.save
+      redirect_to @testimonial, notice: "Your testimonial has been succefully added"
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def testimonial_params
+    params.require(:project).permit(:author,
+                                    :text,
+                                    :video)
   end
 
 end
