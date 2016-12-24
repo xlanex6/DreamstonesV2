f64e94f5fbc14cc5ffd32caf9aefb75f8f99ef39
Alex <alexduval71@gmail.com>
2016-12-24 19:40:47 +0100

    make helper to display double size on testimonial

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,12 @@
 module ApplicationHelper
 
+  def important?(testimonial)
+    if testimonial.important?
+      return "grid-item grid-item--width2"
+    else
+      return "grid-item"
+    end
+  end
+
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161214143429) do
+ActiveRecord::Schema.define(version: 20161224181809) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -67,8 +67,9 @@ ActiveRecord::Schema.define(version: 20161214143429) do
     t.string   "text"
     t.string   "author"
     t.string   "video"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
+    t.boolean  "important",  default: false
   end
 
   create_table "users", force: :cascade do |t|
