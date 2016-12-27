173acbeaf552733785ab4f2205fdc186a5c33255
Alex <alexduval71@gmail.com>
2016-12-27 23:25:47 +0100

    change slider

--- app/assets/javascripts/smooth_scroll.js
+++ app/assets/javascripts/smooth_scroll.js
@@ -17,8 +17,15 @@ $(window).scroll(function() {
         $('#return-to-top').fadeOut(200);   // Else fade out the arrow
     }
 });
-// $('#return-to-top').click(function() {      // When arrow is clicked
-//     $('body,html').animate({
-//         scrollTop : 0                       // Scroll to top of body
-//     }, 500);
+
+// ====== Slider move on Scroll  =============
+// 
+// $(window).scroll(function(){
+//   sliderScroll();
 // });
+//
+// function sliderScroll () {
+//   var wScroll = $(window).scrollTop();
+//
+//   console.log(wScroll);
+// }
