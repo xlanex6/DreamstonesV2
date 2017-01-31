034da8af1bc14bc556c9e8c78af5d4c7586c9ed2
Alex <alexduval71@gmail.com>
2017-01-31 22:44:02 +0100

    sitemap optimsed

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,5 +1,5 @@
 # Set the host name for URL creation
-SitemapGenerator::Sitemap.default_host = "http://www.example.com"
+SitemapGenerator::Sitemap.default_host = "http://www.dreamstones.co "
 
 SitemapGenerator::Sitemap.create do
   # Put links creation logic here.
@@ -19,37 +19,10 @@ SitemapGenerator::Sitemap.create do
   #
   #   add articles_path, :priority => 0.7, :changefreq => 'daily'
   #
-  # Add all articles:
-  #
-  #   Article.find_each do |article|
-  #     add article_path(article), :lastmod => article.updated_at
-  #   end
+  # Add all project:
 
-  # match '/ask-more-information', to: => redirect('/process')
-  # match '/buildinginfrance.html', to: => redirect('/process')
-  # match '/users/dreamstones', to: => redirect('/process')
-  # match '/contact.html', to: => redirect('/process')
-  # match '/building-projects.html', to: => redirect('/process')
-  # match '/projects.html', to: => redirect('/process')
-  # match '/legal-mentions.html', to: => redirect('/process')
-  # match '/italy.html', to: => redirect('/process')
-  # match '/builder-brokers-faqs.html', to: => redirect('/process')
-  # match '/about.html', to: => redirect('/process')
-  # match '/contact-us.html', to: => redirect('/process')
-  # match '/homebuilding.html', to: => redirect('/process')
-  # match '/user/logout', to: => redirect('/process')
-  # match '/evaluation.html', to: => redirect('/process')
-  # match '/dreamstones-services.html', to: => redirect('/process')
-  # match '/renovation.html-0', to: => redirect('/process')
-  # match '/renovation.html', to: => redirect('/process')
-  # match '/faqs.html', to: => redirect('/process')
-  # match '/extensionveranda.html', to: => redirect('/process')
-  # match '/extension.html', to: => redirect('/process')
-  # match '/united-kingdom.html', to: => redirect('/process')
-  # match '/spain.html', to: => redirect('/process')
-  # match '/design-build-process.html', to: => redirect('/process')
-  # match '/design.html', to: => redirect('/process')
-  # match '/preparation.html', to: => redirect('/process')
-  # match '/new-build.html', to: => redirect('/process')
-  # match '/portugal.html', to: => redirect('/process')
-end
+    Project.find_each do |project|
+      add project_path(project), :lastmod => project.updated_at
+    end
+
+SitemapGenerator::Sitemap.ping_search_engines
