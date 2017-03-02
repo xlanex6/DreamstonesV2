a9443a96956ef353b5f7103082d5813014df6cc4
Alex <alexduval71@gmail.com>
2017-03-02 23:33:59 +0100

    Validation model on latest_project

--- app/models/latest_project.rb
+++ app/models/latest_project.rb
@@ -2,10 +2,10 @@ class LatestProject < ApplicationRecord
   has_attachment :main_picture
   has_attachments :photos, maximum: 8
 
-  # validates_presence_of :main_picture,
-  #                       :photos,
-  #                       :title,
-  #                       :area,
-  #                       :category,
-  #                       :size
+  validates_presence_of :main_picture,
+                        :photos,
+                        :title,
+                        :area,
+                        :category,
+                        :size
 end
