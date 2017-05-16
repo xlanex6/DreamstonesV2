20b330977e28bb7c4953631958999d9a2fbc204e
alex duval <alexduval71@gmail.com>
2017-05-16 18:17:12 +0200

    Change for the new typeform

--- app/assets/javascripts/smooth_scroll.js
+++ app/assets/javascripts/smooth_scroll.js
@@ -2,7 +2,9 @@ $(document).ready(function() {
   $('.js-scrollTo').on('click', function() { // Au clic sur un élément
     var page = $(this).attr('href'); // Page cible
     var speed = 750; // Durée de l'animation (en ms)
-    $('html, body').animate( { scrollTop: $(page).offset().top }, speed ); // Go
+    $('html, body').animate({
+      scrollTop: $(page).offset().top
+    }, speed); // Go
     return false;
   });
 });
@@ -11,15 +13,15 @@ $(document).ready(function() {
 
 // ===== Icon qppear for Scroll to Top ====
 $(window).scroll(function() {
-    if ($(this).scrollTop() >= 150) {        // If page is scrolled more than 50px
-        $('#return-to-top').fadeIn(200);    // Fade in the arrow
-    } else {
-        $('#return-to-top').fadeOut(200);   // Else fade out the arrow
-    }
+  if ($(this).scrollTop() >= 150) { // If page is scrolled more than 50px
+    $('#return-to-top').fadeIn(200); // Fade in the arrow
+  } else {
+    $('#return-to-top').fadeOut(200); // Else fade out the arrow
+  }
 });
 
 // ====== Slider move on Scroll  =============
-// 
+//
 // $(window).scroll(function(){
 //   sliderScroll();
 // });
