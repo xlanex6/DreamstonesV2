26f2e6d4098f1ff523abaf999238f3e14f2eebf2
alex duval <alexduval71@gmail.com>
2017-05-18 12:37:58 +0200

    Config generator for Rspec

--- config/application.rb
+++ config/application.rb
@@ -19,9 +19,19 @@ Bundler.require(*Rails.groups)
 
 module DreamstonesV2
   class Application < Rails::Application
-    config.generators do |generate|
-      generate.assets false
+    config.generators do |g|
+      g.assets false
+      g.test_framework :rspec,
+        :fixtures => true,
+        :view_specs => false,
+        :helper_specs => false,
+        :routing_specs => false,
+        :controller_specs => true,
+        :request_specs => true
+      g.fixture_replacement :factory_girl, :dir => "spec/factories"
     end
+
+  end
 
     ### set I18n #####
     config.i18n.default_locale = :en
