207d63ea37a7d50e7bcdb37d597323c0ae283280
alex duval <alexduval71@gmail.com>
2017-05-18 14:09:59 +0200

    BAsic setting for RSpec could run on circleCI

--- config/application.rb
+++ config/application.rb
@@ -19,25 +19,18 @@ Bundler.require(*Rails.groups)
 
 module DreamstonesV2
   class Application < Rails::Application
-    config.generators do |g|
-      g.assets false
-      g.test_framework :rspec,
-        :fixtures => true,
-        :view_specs => false,
-        :helper_specs => false,
-        :routing_specs => false,
-        :controller_specs => true,
-        :request_specs => true
-      g.fixture_replacement :factory_girl, :dir => "spec/factories"
+    config.generators do |generate|
+      generate.assets false
     end
-
-  end
-
-    ### set I18n #####
-    config.i18n.default_locale = :en
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
   end
 end
+
+    ### set I18n #####
+
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration should go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded.
--- spec/factories/projects.rb
+++ spec/factories/projects.rb
@@ -1,5 +1,6 @@
 FactoryGirl.define do
   factory :project do
-    
+    title "My castle"
+    new_buid false
   end
 end
