38f1f9fac9be4dc76af37dc1d039324a16c254a1
alex duval <alexduval71@gmail.com>
2017-08-09 16:44:56 +0200

    Add a thanks page with 6 sec; pause then redirection on riit path

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,7 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home,
                                                   :full_process,
-                                                  :contact,
+                                                  :thanks,
                                                   :legals_mentions ]
 
   def home
